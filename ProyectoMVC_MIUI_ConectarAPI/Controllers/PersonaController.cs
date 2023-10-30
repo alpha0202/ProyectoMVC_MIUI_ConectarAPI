39082319b649c52e35b49ac5746f041c1d621651
edwin harley martinez gomez <alpha0202@gmail.com>
2023-10-30 18:22:16 -0500

    Agregar archivos de proyecto.

---
+++ ProyectoMVC_MIUI_ConectarAPI/Controllers/PersonaController.cs
@@ -0,0 +1,38 @@
+﻿using CapaEntidad;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace ProyectoMVC_MIUI_ConectarAPI.Controllers
+{
+    public class PersonaController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private string _baseUrl;
+
+        public PersonaController(IConfiguration configuration, IHttpClientFactory httpClientFactory)
+        {
+            _baseUrl = configuration["baseUrl"];
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public IActionResult Index()
+        {
+           
+
+            return View();
+        }
+
+        public async Task<List<PersonaCLS>> ListarPersonas()
+        {
+            var cliente = _httpClientFactory.CreateClient();
+            cliente.BaseAddress = new Uri(_baseUrl);
+
+            string strCadena = await cliente.GetStringAsync("/api/persona");
+
+            List<PersonaCLS> lista = JsonSerializer.Deserialize<List<PersonaCLS>>(strCadena);
+
+            return lista;
+        }
+
+    }
+}
---
+++ ProyectoMVC_MIUI_ConectarAPI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProyectoMVC_MIUI_ConectarAPI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
